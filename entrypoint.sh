175b31df198d985170abefea3bb37fc56dbc2189
Alpha-coach <alphacoach@ya.ru>
2025-07-19 15:14:34 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+# Если переменная ZEP_STORE_TYPE задана - экспортируем её как store.type
+if [ ! -z "$ZEP_STORE_TYPE" ]; then
+  export store_type_value="$ZEP_STORE_TYPE"
+  export store_type_name="store.type"
+  export "$store_type_name"="$store_type_value"
+fi
+
 exec /app/zep
