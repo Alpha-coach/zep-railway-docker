f9156fc10cb86ec3492686c6a9f3758b6bc4fe99
Alpha-coach <alphacoach@ya.ru>
2025-07-19 15:28:32 +0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Если переменная ZEP_STORE_TYPE задана - запускаем через env с нужной переменной
-if [ ! -z "$ZEP_STORE_TYPE" ]; then
-  exec env store.type="$ZEP_STORE_TYPE" /app/zep
-else
-  exec /app/zep
-fi
